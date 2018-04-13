ef9cda123c7ddbd6ce7d39f6befee982a2b76730
civrev <civrev@gmail.com>
2018-04-13 00:28:00 -0400

    more setup fixing

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setup(
     name = 'rlrisk',
     version = '0.9.5',
     description = 'A reinforcement learning environment based off the board game Risk',
+    long_description = open('README.rst').read(),
     author = 'Christian Watts',
     author_email = 'civrev@gmail.com',
     license = '',
     url='https://github.com/civrev/rlrisk',
     packages = find_packages(),
-    package_data={'rlrisk': ['environment/*.bmp','*.txt','*.md']},
+    package_data={'rlrisk': ['environment/*.bmp','*.txt','*.rst']},
     install_requires = [
         'numpy>=1.14.1',
         'pygame>=1.9.3']
